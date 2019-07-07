8bd32df0726cb44fb999a30df13c2edebe45c39a
Simao Gomes Viana <xdevs23@outlook.com>
2019-07-07 17:43:42 +0200

    picgen: fix the mess that atom caused

--- picgen.py
+++ picgen.py
@@ -25,27 +25,31 @@ def main():
 
     parser = argparse.ArgumentParser(description='Generate a random image')
     parser.add_argument('--gen-size',
-    help='Size of image to generate (not the actual output resolution) (default: %sx%s)' % (xInputSize, yInputSize))
+            help='Size of image to generate (not the actual output resolution) (default: %sx%s)' % (xInputSize, yInputSize))
     parser.add_argument('--resolution',
-    help='Output resolution (default: %sx%s)' % (xOutputSize, yOutputSize))
+            help='Output resolution (default: %sx%s)' % (xOutputSize, yOutputSize))
     parser.add_argument('--mode',
-    help='Pattern/Mode (available: boring, fancy) (default: %s)' % mode)
-    parser.add_argument('--blur', help="Specify to enable", action='store_true')
-    parser.add_argument('--blur-radius', type=int, help="Blur radius (only has effect if --blur is set) (default: %s)" % blurSigma)
-    parser.add_argument('--save-to', help="Save image to the specified file, otherwise open in default viewer")
-    parser.add_argument('--open-in-viewer', help="Open image in viewer regardless of the --save-to argument", action="store_true")
+            help='Pattern/Mode (available: boring, fancy) (default: %s)' % mode)
+    parser.add_argument('--blur',
+            help="Specify to enable", action='store_true')
+    parser.add_argument('--blur-radius', type=int,
+            help="Blur radius (only has effect if --blur is set) (default: %s)" % blurSigma)
+    parser.add_argument('--save-to',
+            help="Save image to the specified file, otherwise open in default viewer")
+    parser.add_argument('--open-in-viewer',
+            help="Open image in viewer regardless of the --save-to argument", action="store_true")
 
     args = parser.parse_args()
 
     if args.gen_size is not None:
-    split = str.split(args.gen_size, 'x')
-    xInputSize = int(split[0])
-    yInputSize = int(split[1])
+        split = str.split(args.gen_size, 'x')
+        xInputSize = int(split[0])
+        yInputSize = int(split[1])
 
     if args.resolution is not None:
-    split = str.split(args.resolution, 'x')
-    xOutputSize = int(split[0])
-    yOutputSize = int(split[1])
+        split = str.split(args.resolution, 'x')
+        xOutputSize = int(split[0])
+        yOutputSize = int(split[1])
 
     if args.mode is not None: mode = args.mode
     if args.blur is not None: enableBlur = args.blur
@@ -68,59 +72,59 @@ def main():
     downscale = np.zeros( (yInputSize, xInputSize, colorsPerPixel), dtype=np.uint8 )
 
     for y in range(len(downscale)):
-    sys.stdout.write("\rGenerating %s out of %s" % (y + 1, len(downscale)))
-    sys.stdout.flush()
-    if mode == fancyMode:
-    for x in range(len(downscale[y])):
-    downscale[y,x] = [random.randint(1,4) * 64 - 20, random.randint(1,4) * 64 - 20, random.randint(1,4) * 64 - 20]
-    elif mode == boringMode:
-    for x in range(len(downscale[y])):
-    downscale[y,x] = [random.randint(0,255), random.randint(0,255), random.randint(0,255)]
+        sys.stdout.write("\rGenerating %s out of %s" % (y + 1, len(downscale)))
+        sys.stdout.flush()
+        if mode == fancyMode:
+            for x in range(len(downscale[y])):
+                downscale[y,x] = [random.randint(1,4) * 64 - 20, random.randint(1,4) * 64 - 20, random.randint(1,4) * 64 - 20]
+        elif mode == boringMode:
+            for x in range(len(downscale[y])):
+                downscale[y,x] = [random.randint(0,255), random.randint(0,255), random.randint(0,255)]
 
     print()
 
     data = downscale
 
     if xInputSize != xOutputSize or yInputSize != yOutputSize:
-    data = np.zeros( (yOutputSize, xOutputSize, colorsPerPixel), dtype=np.uint8 )
-    print("Upscaling...")
+        data = np.zeros( (yOutputSize, xOutputSize, colorsPerPixel), dtype=np.uint8 )
+        print("Upscaling...")
 
-    upscaleFactorX = len(data[0]) / len(downscale[0])
-    upscaleFactorY = len(data) / len(downscale)
+        upscaleFactorX = len(data[0]) / len(downscale[0])
+        upscaleFactorY = len(data) / len(downscale)
 
-    print("  Upscale factor X: %s" % upscaleFactorX)
-    print("  Upscale factor Y: %s" % upscaleFactorY)
+        print("  Upscale factor X: %s" % upscaleFactorX)
+        print("  Upscale factor Y: %s" % upscaleFactorY)
 
-    for y in range(len(data)):
-    sys.stdout.write("\rUpscaling %s out of %s" % (y + 1, len(data)))
-    sys.stdout.flush()
-    for x in range(len(data[y])):
-    yIx = math.floor(y / upscaleFactorY)
-    # Check if we're still in bounds
-    if yIx > len(downscale): break
-    xIx = math.floor(x / upscaleFactorX)
-    # Check if we're still in bounds
-    if xIx > len(downscale[yIx]): break
-    data[y,x] = downscale[yIx][xIx]
+        for y in range(len(data)):
+            sys.stdout.write("\rUpscaling %s out of %s" % (y + 1, len(data)))
+            sys.stdout.flush()
+            for x in range(len(data[y])):
+                yIx = math.floor(y / upscaleFactorY)
+                # Check if we're still in bounds
+                if yIx > len(downscale): break
+                xIx = math.floor(x / upscaleFactorX)
+                # Check if we're still in bounds
+                if xIx > len(downscale[yIx]): break
+                data[y,x] = downscale[yIx][xIx]
 
-    print()
+        print()
 
     if enableBlur:
-    print("Applying blur...")
-    # Separate r, g, b
-    b, g, r = data[:, :, 0], data[:, :, 1], data[:, :, 2]
+        print("Applying blur...")
+        # Separate r, g, b
+        b, g, r = data[:, :, 0], data[:, :, 1], data[:, :, 2]
 
-    # Blur it
-    r = gaussian_filter(r, sigma=blurSigma)
-    g = gaussian_filter(g, sigma=blurSigma)
-    b = gaussian_filter(b, sigma=blurSigma)
+        # Blur it
+        r = gaussian_filter(r, sigma=blurSigma)
+        g = gaussian_filter(g, sigma=blurSigma)
+        b = gaussian_filter(b, sigma=blurSigma)
 
-    # Put it back together
-    data[..., 0] = b
-    data[..., 1] = g
-    data[..., 2] = r
+        # Put it back together
+        data[..., 0] = b
+        data[..., 1] = g
+        data[..., 2] = r
 
-    print()
+        print()
 
     print("Constructing final image")
     image = Image.fromarray(data)
@@ -128,13 +132,13 @@ def main():
     print()
 
     if openInViewer:
-    print("Opening image")
-    image.show() # View in default viewer
+        print("Opening image")
+        image.show() # View in default viewer
 
     if saveTo is not None:
-    print("Saving image")
-    image.save(saveTo) # Save to filesystem
-    print("Saved")
+        print("Saving image")
+        image.save(saveTo) # Save to filesystem
+        print("Saved")
 
     print()
 
